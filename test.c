f9d237366442593be8076a83db364a2574ec6b8f
4lovelydong <4lovelydong@naver.com>
2014-04-17 11:20:02 +0900

    misc. update
    		indentation with line spacing

--- test.c
+++ test.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 int main(int argc, char *argv[])
 {
-	fputs("\033[24;75H", stdout);
-	fputs("\033[24;75HHello\n",stdout);
+	fputs("\033[23;75H", stdout);
+	fputs("\033[23;75HHello\n",stdout);
 
 	return 0;
 }
